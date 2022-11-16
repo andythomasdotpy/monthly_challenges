b6f6015894244373c4189effe5b5cd33247db8d0
Andy <andythomas.py@gmail.com>
2022-11-16 13:03:26 -0600

    first commit on new branch

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.january , name="january"),
-    path("february", views.february, name="february"),
-    path("march", views.march, name="march"),
-    path("<month>", views.monthly_challenge, name="monthly_challenge"),
+    path("<int:month>", views.monthly_challenge_by_number, name="monthly_challenge_by_number"),
+    path("<str:month>", views.monthly_challenge, name="monthly_challenge"),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+monthly_challenges = {
+    "january": "Hi, I'm January!",
+    "february": "Hi, I'm February!",
+    "march": "Hi, I'm March!",
+    "april": "Hi, I'm April!",
+    "may": "Hi, I'm May!",
+    "june": "Hi, I'm June!",
+    "july": "Hi, I'm July!",
+    "august": "Hi, I'm August!",
+    "september": "Hi, I'm September!",
+    "october": "Hi, I'm October!",
+    "november": "Hi, I'm November!",
+    "december": "Hi, I'm December!",
+}
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("Hi, January!")
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
 
 
-def february(request):
-    return HttpResponse("Hello, February!")
-
-def march(request):
-    return HttpResponse("Hi, March")
-
-
-def monthly_challenge(request):
-    return HttpResponse()
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Hi, I'm January!"
+    elif month == "february":
+        challenge_text = "Hi, I'm February!"
+    elif month == "march":
+        challenge_text = "Hi, I'm March!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
