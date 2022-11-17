c83f3b3657af5693a988721c3577cadc1f2b3adf
Andy <andythomas.py@gmail.com>
2022-11-16 21:23:32 -0600

    end of section 3

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Hi, I'm January!",
@@ -18,19 +19,40 @@ monthly_challenges = {
 
 # Create your views here.
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href='{month_path}'>{month.capitalize()}</a></li>"
+
+    months_list = f"""
+        <ul>
+            {list_items}
+        </ul>
+    """
+
+    return HttpResponse(months_list)
+
+
 
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    try:
+        monthly_keys_list = list(monthly_challenges.keys())
+        redirect_month_name = monthly_keys_list[month - 1]
+        redirect_path = reverse("month-challenge", args=[redirect_month_name])
+        return HttpResponseRedirect(redirect_path)
+    except:
+        return HttpResponseNotFound("Sorry, month not found.")
+
+    
 
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Hi, I'm January!"
-    elif month == "february":
-        challenge_text = "Hi, I'm February!"
-    elif month == "march":
-        challenge_text = "Hi, I'm March!"
-    else:
-        return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound("<h1>Can't find that month</h1>")
---
+++ sandbox.py
@@ -0,0 +1,14 @@
+my_dict = {
+    "hello": "hi, hello",
+    "bye": "hi, bye",
+    "soon": "hi, soon",
+}
+
+keys_returned = list(my_dict.keys())
+print(keys_returned)
+
+# my_int = keys_returned.index("hello")
+# print(my_int)
+
+print(keys_returned[0])
+
